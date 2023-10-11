7062a07fac745d21824d5c97f92fbd1d0ed81189
Tomas <tomasmedero92@gmail.com>
2023-10-11 19:51:20 -0300

    markers add

--- src/components/Mapa.jsx
+++ src/components/Mapa.jsx
@@ -1,35 +1,34 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import { hospitalInfo } from "../helpers";
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import { hospitalInfo } from '../helpers'
 
 export const Mapa = () => {
   return (
-    <div className="max-h-100 max-w-100">
+    <div className='max-h-100 max-w-100'>
       <MapContainer
-        center={[-34.590214, -58.452091]}
+        center={[-34.617214, -58.453091]}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        <Marker position={[-34.628847360388136, -58.377550848844308]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-        <Marker position={[-34.59419197263928, -58.412070076945632]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-        <Marker position={[-34.584528335750541, -58.402727654782787]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+
+        {hospitalInfo.map((hospital) => (
+          <>
+            <Marker position={hospital.geometry.coordinates}>
+              <Popup>
+                <p>Nombre: {hospital.properties.NOMBRE}</p>
+                <p>
+                  Calle: {hospital.properties.CALLE}{' '}
+                  {hospital.properties.ALTURA}
+                </p>
+              </Popup>
+            </Marker>
+          </>
+        ))}
       </MapContainer>
     </div>
-  );
-};
+  )
+}
