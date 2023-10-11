341b3bc7df9466b8d128a9ba2b7d2637b1ad01e5
Tomas <tomasmedero92@gmail.com>
2023-10-11 20:22:17 -0300

    about us add

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,3 @@
+export const AboutUs = () => {
+  return <div>Aca va todo el HTML del About US</div>
+}
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { App } from '../App'
 import { Navbar } from '../components'
+import { AboutUs } from '../pages'
 
 export const AppRouter = () => {
   return (
@@ -9,6 +10,7 @@ export const AppRouter = () => {
         <Navbar />
         <Routes>
           <Route path='/*' element={<App />} />
+          <Route path='/about' element={<AboutUs />} />
           <Route path='/*' element={<Navigate to='/' />} />
         </Routes>
       </div>
