61f28fb2eba68a366a1b8ea06bdc5d9bfb05de6e
Leandro <leanf94@gmail.com>
2023-10-12 00:20:30 -0300

    add-vitanav-marker

--- src/components/Mapa.jsx
+++ src/components/Mapa.jsx
@@ -1,16 +1,16 @@
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-import { hospitalInfo } from '../helpers'
-import { Icon } from 'leaflet'
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { hospitalInfo } from "../helpers";
+import { Icon } from "leaflet";
 
 export const Mapa = () => {
   const customIcon = new Icon({
-    iconUrl: ' /markerPosition.png',
+    iconUrl: " /markerPosition.svg",
     iconSize: [34, 34],
-  })
+  });
 
   return (
-    <div className='max-h-100 max-w-100'>
+    <div className="max-h-100 max-w-100">
       <MapContainer
         center={[-34.617214, -58.453091]}
         zoom={13}
@@ -18,7 +18,7 @@ export const Mapa = () => {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         {hospitalInfo.map((hospital) => (
@@ -26,11 +26,11 @@ export const Mapa = () => {
             <Marker position={hospital.geometry.coordinates} icon={customIcon}>
               <Popup>
                 <p>
-                  <span className='font-extrabold'>Nombre:</span>{' '}
+                  <span className="font-extrabold">Nombre:</span>{" "}
                   {hospital.properties.NOMBRE}
                 </p>
                 <p>
-                  <span className='font-extrabold'>Calle:</span>{' '}
+                  <span className="font-extrabold">Calle:</span>{" "}
                   {hospital.properties.CALLE} {hospital.properties.ALTURA}
                 </p>
               </Popup>
@@ -39,5 +39,5 @@ export const Mapa = () => {
         ))}
       </MapContainer>
     </div>
-  )
-}
+  );
+};
